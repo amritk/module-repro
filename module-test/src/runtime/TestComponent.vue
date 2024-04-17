5481b3c9c5490a4e25a6910d6f8e17a6ca2b6f69
Amrit <amrit@scalar.com>
2024-04-17 15:21:52 -0700

    chore: initial commit

---
+++ module-test/src/runtime/TestComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is but a test component from the module</div>
+</template>
+
+<script setup lang="ts">
+import createDebug from "debug";
+
+const debug = createDebug("module-test");
+console.log({ debug });
+debug("This is but a test log");
+</script>
